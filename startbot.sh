7cb9b30f09641b2e0f298ac1f33ce1c0d7be85cc
MitchelPaulin <mitchel0022@gmail.com>
2022-01-23 18:53:22 -0500

    Trying to compile the engine in heroku

--- startbot.sh
+++ startbot.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+git clone https://github.com/MitchelPaulin/Walleye --depth=1
+cd Walleye
+cargo build --release
+mv target/release/walleye ../engines/walleye
+cd ..
+rm -rf Walleye
 chmod a+x engines/walleye
 ls engines -l
 python3 lichess-bot.py -u
