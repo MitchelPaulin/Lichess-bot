584d42c6f65d58301b118f155fc53b2715f2734f
MitchelPaulin <mitchel0022@gmail.com>
2022-01-23 19:11:36 -0500

    Adding soruce to try and ensure cargo is in scope

--- startbot.sh
+++ startbot.sh
@@ -2,6 +2,7 @@
 curl https://sh.rustup.rs -sSf > install_cargo.sh
 chmod 777 install_cargo.sh
 ./install_cargo.sh -y
+source $HOME/.cargo/env
 git clone https://github.com/MitchelPaulin/Walleye --depth=1
 cd Walleye
 cargo build --release
